fa62b1c9532a6319cb5ea6940be9985e6af3b696
55555-Jyeon <jyeon380516@gmail.com>
2023-06-02 00:53:41 +0900

    JS:modification

--- js/qna.js
+++ js/qna.js
@@ -1,25 +1,39 @@
 
 /* QNA */
 /* 자주 묻는 질문 */
+const questionList = document.querySelector('#question-list');
+
 var answer = document.querySelectorAll('.answer');
 
-const answer01 = document.querySelector('#answer01');
-const answer02 = document.querySelector('#answer02');
-const answer03 = document.querySelector('#answer03');
-const answer04 = document.querySelector('#answer04');
-const answer05 = document.querySelector('#answer05');
-const answer06 = document.querySelector('#answer06');
+/* answer box */
+const answer01 = questionList.querySelector('#answer01');
+const answer02 = questionList.querySelector('#answer02');
+const answer03 = questionList.querySelector('#answer03');
+const answer04 = questionList.querySelector('#answer04');
+const answer05 = questionList.querySelector('#answer05');
+const answer06 = questionList.querySelector('#answer06');
+
+/* question box에 있는 + 이미지 */
+const plus01 = questionList.querySelector('.plus-01');
+const plus02 = questionList.querySelector('.plus-02');
+const plus03 = questionList.querySelector('.plus-03');
+const plus04 = questionList.querySelector('.plus-04');
+const plus05 = questionList.querySelector('.plus-05');
+const plus06 = questionList.querySelector('.plus-06');
 
 const form = document.querySelector('#moveonClick');
+
 
 function shortAnswer01(){
 
     if(answer01.style.display === 'none'){
         answer01.style.display = 'block';
         form.style.marginTop = '11%';
+        plus01.style.transform = 'rotate(45deg)';
     } else {
         answer01.style.display = 'none';
         form.style.marginTop = '0';
+        plus01.style.transform = 'rotate(0deg)';
     }
 }
 
@@ -28,9 +42,11 @@ function shortAnswer02(){
     if(answer02.style.display === 'none'){
         answer02.style.display = 'block';
         form.style.marginTop = '11%';
+        plus02.style.transform = 'rotate(45deg)';
     } else {
         answer02.style.display = 'none';
         form.style.marginTop = '0';
+        plus02.style.transform = 'rotate(0deg)';
     }
 }
 function shortAnswer03(){
@@ -38,9 +54,11 @@ function shortAnswer03(){
     if(answer03.style.display === 'none'){
         answer03.style.display = 'block';
         form.style.marginTop = '8%';
+        plus03.style.transform = 'rotate(45deg)';
     } else {
         answer03.style.display = 'none';
         form.style.marginTop = '0';
+        plus03.style.transform = 'rotate(0deg)';
     }
 }
 
@@ -49,9 +67,11 @@ function shortAnswer04(){
     if(answer04.style.display === 'none'){
         answer04.style.display = 'block';
         form.style.marginTop = '13%';
+        plus04.style.transform = 'rotate(45deg)';
     } else {
         answer04.style.display = 'none';
         form.style.marginTop = '0';
+        plus04.style.transform = 'rotate(0deg)';
     }
 }
 function shortAnswer05(){
@@ -59,9 +79,11 @@ function shortAnswer05(){
     if(answer05.style.display === 'none'){
         answer05.style.display = 'block';
         form.style.marginTop = '8%';
+        plus05.style.transform = 'rotate(45deg)';
     } else {
         answer05.style.display = 'none';
         form.style.marginTop = '0';
+        plus05.style.transform = 'rotate(0deg)';
     }
 }
 
@@ -70,8 +92,10 @@ function shortAnswer06(){
     if(answer06.style.display === 'none'){
         answer06.style.display = 'block';
         form.style.marginTop = '12%';
+        plus06.style.transform = 'rotate(45deg)';
     } else {
         answer06.style.display = 'none';
         form.style.marginTop = '0';
+        plus06.style.transform = 'rotate(0deg)';
     }
 }
